ce960e253b2f2b54dbbf7292a35a78d332e25acc
zzlk <2418897+zzlk@users.noreply.github.com>
2022-01-31 21:12:56 -0800

    add bindings for uchardet

---
+++ build.rs
@@ -0,0 +1,44 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::Path;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        Path::new(&dir).join("uchardet/src").display()
+    );
+    println!("cargo:rustc-link-lib=uchardet");
+    // println!("cargo:rustc-link-lib=storm");
+    // println!("cargo:rustc-link-lib=bz2");
+    // println!("cargo:rustc-link-lib=z");
+    // println!("cargo:rustc-link-lib=stdc++");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=src/wrapper.hpp");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("src/wrapper.hpp")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ src/wrapper.hpp
@@ -0,0 +1,1 @@
+#include "../uchardet/src/uchardet.h"
